0fe0afb4fd9a301ae94cbce82431b7714853fec6
Rahul.Bhave <rahul.bhave@qxf2.com>
2020-05-28 23:09:34 +0530

    added both aproaches for msg filter

--- utils/sqs_messages.py
+++ utils/sqs_messages.py
@@ -34,24 +34,41 @@ class sqsmessage():
     # declaring templates directory
     template_directory = 'templates'
 
-    def get_messages_from_queue(self,queue_url,key):
+    def get_messages_from_queue(self,queue_url):
         """
         Generates messages from an SQS queue.
         :param queue_url: URL of the SQS queue to drain.
         :return: The AWS response
         """
+
         _logger = logging.getLogger(__name__)
         _logger.setLevel(logging.DEBUG)
         sqs_client = boto3.client('sqs')
         queue = boto3.resource('sqs').get_queue_by_name(QueueName=queue_url)
         while True:
-            messages = sqs_client.receive_message(QueueUrl=queue.url,AttributeNames=['key'])
+            messages = sqs_client.receive_message(QueueUrl=queue.url, MessageAttributeNames=['Price'])
             if 'Messages' in messages:
                 for message in messages['Messages']:
-                    print(message['Body'])
+                        print(message['Body'])
             else:
                 print('Queue is now empty')
                 break
+
+        """
+        # Approach to filterout message based on key to be discarded if not useful
+        sqs_client = boto3.client('sqs')
+        queue = boto3.resource('sqs').get_queue_by_name(QueueName=queue_url)
+        for message in sqs_client.receive_messages(MessageAttributeNames=['Author']):
+            author_text = ''
+            if message.message_attributes is not None:
+                author_name = message.message_attributes.get('Author').get('StringValue')
+                if author_name is not None:
+
+                    author_text = ' ({0})'.format(author_name)
+                    print('Hello, {0}!{1}'.format(message.body, author_text))
+        """
+
+
 
     def send_message_to_queue(self,queue_url):
         """
@@ -77,8 +94,8 @@ if __name__=='__main__':
     sqsmessage_obj = sqsmessage()
     ap = argparse.ArgumentParser()
     ap.add_argument("--queue_url", required=True, help="Queue URL")
-    ap.add_argument("--key", required=True, help="Attribute Name")
     args = ap.parse_args()
-    sqsmessage_obj.get_messages_from_queue(args.queue_url,args.key)
+    sqsmessage_obj.get_messages_from_queue(args.queue_url)
+
 else:
         print('ERROR: Received incorrect comand line input arguments')
